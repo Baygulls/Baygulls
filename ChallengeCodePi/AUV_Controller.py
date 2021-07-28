1543ac2825e0221f686282a5deff44f372365514
Eloise Zeng <eloise.zeng@gmail.com>
2021-07-28 21:05:17 +0100

    deleted log files, fixed how to calculate delta_angle in simple way

--- ChallengeCodePi/AUV_Controller.py
+++ ChallengeCodePi/AUV_Controller.py
@@ -98,7 +98,8 @@ class AUVController():
     def __select_angle(self):
         # determine the angle between current and desired heading
         delta_angle = self.__desired_heading - self.__heading
-        delta_angle = delta_angle ** 1.25 / 1.5
+        # delta_angle = delta_angle ** 1.25 / 1.5
+        delta_angle *= 2
         delta_angle %= 360
         
         if delta_angle > 180: # angle too big, go the other way!
@@ -112,5 +113,5 @@ class AUVController():
             
         elif delta_angle < -25:
             delta_angle = -25
-            
+        self.__logger.info(f"delta angle: {delta_angle}")
         return delta_angle
