21587f0352312cf7ac948d64ca616a6159514e80
Steven Gao <stevenruidigao@gmail.com>
2021-07-29 15:06:56 +0100

    fix checksum again

--- ChallengeCodePi/BWSI_BackSeat.py
+++ ChallengeCodePi/BWSI_BackSeat.py
@@ -28,9 +28,9 @@ from Sandshark_Interface import SandsharkClient
 def valid_checksum(msg):
     fields = msg.split('*')
     cmd = fields[0][1:]
-    expected = str(hex(BluefinMessages.checksum(cmd))[:2])
+    expected = str(hex(BluefinMessages.checksum(cmd))[2:])
     
-    if expected.upper() != fields[1][0:2].upper():
+    if expected.upper() != fields[1][:2].upper():
         print(f"cmd = {cmd}")
         print(f"{expected} != {fields[1]}")
         return False
